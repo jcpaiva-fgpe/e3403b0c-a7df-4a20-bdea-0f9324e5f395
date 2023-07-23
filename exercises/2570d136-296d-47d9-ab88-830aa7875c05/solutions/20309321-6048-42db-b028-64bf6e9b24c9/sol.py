16ec93621b8f0e9e7984feb7f2285276bcc7f531
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 09:27:28 +0100

    updated file exercises/2570d136-296d-47d9-ab88-830aa7875c05/solutions/20309321-6048-42db-b028-64bf6e9b24c9/sol.py

--- exercises/2570d136-296d-47d9-ab88-830aa7875c05/solutions/20309321-6048-42db-b028-64bf6e9b24c9/sol.py
+++ exercises/2570d136-296d-47d9-ab88-830aa7875c05/solutions/20309321-6048-42db-b028-64bf6e9b24c9/sol.py
@@ -1,3 +1,6 @@
 from re import findall 
 txt = """mapy - https://www.google.com/maps, Wimbledon - https://www.youtube.com/Wimbledon, copilot - https://github.com/features/copilot, Verne - https://lubimyczytac.pl/autor/6737/juliusz-verne"""
 print([x[0] for x in findall("(https?:\/\/([\w_-]+\.)+[\w_-]+(\/[\w_-]+)*\/?)", txt)])
+
+# exp output
+# ['https://www.google.com/maps', 'https://www.youtube.com/Wimbledon', 'https://github.com/features/copilot', 'https://lubimyczytac.pl/autor/6737/juliusz-verne']
