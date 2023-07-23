572ce0bc8a1a27d2177d1183f8a896cf31be5ab6
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 08:30:16 +0100

    updated file exercises/6ac53193-5a36-462d-94e5-da655a134fb4/skeletons/9c17a34c-50ed-47fc-a686-04c3a9604074/s.py

--- exercises/6ac53193-5a36-462d-94e5-da655a134fb4/skeletons/9c17a34c-50ed-47fc-a686-04c3a9604074/s.py
+++ exercises/6ac53193-5a36-462d-94e5-da655a134fb4/skeletons/9c17a34c-50ed-47fc-a686-04c3a9604074/s.py
@@ -1,2 +1,5 @@
 from re import search
 print(search("{{gap}}", "Jan Kowalski urodzony 12.12.2001 w Dobiegniewie, syn Klary i Melchiora."))
+
+# exp output
+# <re.Match object; span=(22, 32), match='12.12.2001'>
