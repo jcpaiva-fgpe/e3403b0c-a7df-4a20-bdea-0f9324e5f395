bcd44beac00035c7f97a8db589a0c42eb2c04760
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 23:41:52 +0100

    created file exercises/c6f23446-4ced-439c-993b-7bf13d51ac29/skeletons/5414ff17-1b36-47c1-bc34-2b3938a64e81/s.py

---
+++ exercises/c6f23446-4ced-439c-993b-7bf13d51ac29/skeletons/5414ff17-1b36-47c1-bc34-2b3938a64e81/s.py
@@ -0,0 +1,11 @@
+from re import findall, {{gap}}
+txt="""O! cichy jestem jak wy! o Atrydzi!
+Których popioły śpią pod świerszczów strażą —
+Ani mię teraz moja małość wstydzi,
+Ani się myśli tak jak orły ważą.
+Głęboko jestem pokorny i cichy
+Tu, w tym grobowcu sławy, zbrodni, pychy!"""
+print(findall(r"{{gap}}", txt, M))
+
+# exp output
+# ['Atrydzi', 'strażą', 'wstydzi', 'ważą', 'cichy', 'pychy']
