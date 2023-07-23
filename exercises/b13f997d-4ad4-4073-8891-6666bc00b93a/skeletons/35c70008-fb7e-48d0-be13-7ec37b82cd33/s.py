010fee4c895ec2ed11dec36410137db6a27d41c6
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 09:36:49 +0100

    updated file exercises/b13f997d-4ad4-4073-8891-6666bc00b93a/skeletons/35c70008-fb7e-48d0-be13-7ec37b82cd33/s.py

---
+++ exercises/b13f997d-4ad4-4073-8891-6666bc00b93a/skeletons/35c70008-fb7e-48d0-be13-7ec37b82cd33/s.py
@@ -0,0 +1,3 @@
+from re import findall 
+txt = """Administrator - konto@usz.edu.pl, Prof. Łyczko - grzegorz.lyczko@usz.edu.pl, pomoc - help-desk@coca-cola.pl, sklep - elektryczne_owce@allegro.pl"""
+print([x[0] for x in findall("({{gap}}@{{gap}}", txt)])
