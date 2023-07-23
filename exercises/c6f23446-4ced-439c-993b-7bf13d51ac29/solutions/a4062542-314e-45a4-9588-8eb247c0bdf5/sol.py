2dc76641de10e3e39a077a34510de76911532e9b
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 23:42:30 +0100

    created file exercises/c6f23446-4ced-439c-993b-7bf13d51ac29/solutions/a4062542-314e-45a4-9588-8eb247c0bdf5/sol.py

---
+++ exercises/c6f23446-4ced-439c-993b-7bf13d51ac29/solutions/a4062542-314e-45a4-9588-8eb247c0bdf5/sol.py
@@ -0,0 +1,8 @@
+from re import findall, M
+txt="""O! cichy jestem jak wy! o Atrydzi!
+Których popioły śpią pod świerszczów strażą —
+Ani mię teraz moja małość wstydzi,
+Ani się myśli tak jak orły ważą.
+Głęboko jestem pokorny i cichy
+Tu, w tym grobowcu sławy, zbrodni, pychy!"""
+print(findall(r"(\w+)\W*?$", txt, M)
