e18a9874ad5a21e073bdc5b3f2098203dc705ea4
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-23 23:54:21 +0100

    created file exercises/df3834f9-07b5-4a4c-b0d8-7af0020fa776/skeletons/7e297a80-5b54-445d-b113-053f0580e72f/s.py

---
+++ exercises/df3834f9-07b5-4a4c-b0d8-7af0020fa776/skeletons/7e297a80-5b54-445d-b113-053f0580e72f/s.py
@@ -0,0 +1,11 @@
+from re import findall
+txt="""Dzisiaj na wielkiem morzu obłąkany,
+Sto mil od brzegu i sto mil przed brzegiem,
+Widziałem lotne w powietrzu bociany
+Długim szeregiem.
+Żem je znał kiedyś na polskim ugorze,
+Smutno mi, Boże!"""
+print(findall(r"{{gap}}", txt))
+
+# exp output
+# ['brzegu', 'brzegiem']
