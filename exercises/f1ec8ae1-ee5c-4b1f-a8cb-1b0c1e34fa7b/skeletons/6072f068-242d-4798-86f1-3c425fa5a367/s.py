bca8b5d0c8c817a70f599409a8f4bd58bb2fdb40
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 10:22:15 +0100

    created file exercises/f1ec8ae1-ee5c-4b1f-a8cb-1b0c1e34fa7b/skeletons/6072f068-242d-4798-86f1-3c425fa5a367/s.py

---
+++ exercises/f1ec8ae1-ee5c-4b1f-a8cb-1b0c1e34fa7b/skeletons/6072f068-242d-4798-86f1-3c425fa5a367/s.py
@@ -0,0 +1,29 @@
+from re import sub
+txt = """1. Wstęp
+2. O obrotach sfer niebieskich
+5. Zaginiony w akcji
+6. Czary i mary
+3. Ratatuj powraca
+4. Nigdy więcej sera
+7. Spleśniały bochenek
+9. Kto zjadł mój plecak?
+10. Szczęśliwe zakończenie
+11. Epilog"""
+
+def licznik(s):
+	licznik.n += 1
+	return str({{gap[0]}}) + '. '
+licznik.n = 0
+print(sub(r"{{gap[1]}}", licznik, txt))
+
+# exp output
+# 1. Wstęp
+# 2. O obrotach sfer niebieskich
+# 3. Zaginiony w akcji
+# 4. Czary i mary
+# 5. Ratatuj powraca
+# 6. Nigdy więcej sera
+# 7. Spleśniały bochenek
+# 8. Kto zjadł mój plecak?
+# 9. Szczęśliwe zakończenie
+# 10. Epilog
