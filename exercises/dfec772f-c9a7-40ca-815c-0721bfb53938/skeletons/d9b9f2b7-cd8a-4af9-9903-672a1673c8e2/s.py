68bc60467d3930f537cbbb7db88da689042ea2d2
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:59:45 +0100

    updated file exercises/dfec772f-c9a7-40ca-815c-0721bfb53938/skeletons/d9b9f2b7-cd8a-4af9-9903-672a1673c8e2/s.py

--- exercises/dfec772f-c9a7-40ca-815c-0721bfb53938/skeletons/d9b9f2b7-cd8a-4af9-9903-672a1673c8e2/s.py
+++ exercises/dfec772f-c9a7-40ca-815c-0721bfb53938/skeletons/d9b9f2b7-cd8a-4af9-9903-672a1673c8e2/s.py
@@ -1,3 +1,6 @@
 from re import sub
 txt = "Jan Maria Rokita nie spotkał się nigdy z Johnem Fitzgeraldem Kennedym"
 print(sub(r"{{gap}}", r"\1 \2. \3", txt))
+
+# exp output
+# Jan M. Rokita nie spotkał się nigdy z Johnem F. Kennedym
