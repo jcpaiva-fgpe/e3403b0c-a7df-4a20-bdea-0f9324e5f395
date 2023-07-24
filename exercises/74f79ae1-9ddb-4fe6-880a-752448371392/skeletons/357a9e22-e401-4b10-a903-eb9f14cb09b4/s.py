c6a1dbf10370f094085915f1f60a1f9dfa84ab69
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:35:15 +0100

    updated file exercises/74f79ae1-9ddb-4fe6-880a-752448371392/skeletons/357a9e22-e401-4b10-a903-eb9f14cb09b4/s.py

--- exercises/74f79ae1-9ddb-4fe6-880a-752448371392/skeletons/357a9e22-e401-4b10-a903-eb9f14cb09b4/s.py
+++ exercises/74f79ae1-9ddb-4fe6-880a-752448371392/skeletons/357a9e22-e401-4b10-a903-eb9f14cb09b4/s.py
@@ -5,3 +5,10 @@ txt = """1. Rębajło, Heliodor - 18,5 s
 4. Dziamdziara, Gleb - 20,1 s
 5. Skowronek, Zdzisław - 20,5 s"""
 print(sub(r"{{gap}}", r"\2 \1", txt))
+
+# exp output
+# 1. Heliodor Rębajło - 18,5 s
+# 2. Apolinary Dziureczko - 19,2 s
+# 3. Szymon Truteń - 19,3 s
+# 4. Gleb Dziamdziara - 20,1 s
+# 5. Zdzisław Skowronek - 20,5 s
