c592ad931175ae1deb10a390808b93e30b22c21c
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:29:57 +0100

    updated file exercises/799a6bf5-25c1-441e-8de4-1812e05e6cee/skeletons/7db37a68-b9d6-42b3-9947-be1bd29eeef4/s.py

--- exercises/799a6bf5-25c1-441e-8de4-1812e05e6cee/skeletons/7db37a68-b9d6-42b3-9947-be1bd29eeef4/s.py
+++ exercises/799a6bf5-25c1-441e-8de4-1812e05e6cee/skeletons/7db37a68-b9d6-42b3-9947-be1bd29eeef4/s.py
@@ -1,3 +1,6 @@
 from re import sub
 txt = "Inflacja średnioroczna wzrosła wtedy do 40% [Henrykowski 1962], a ceny bochenka chleba przekroczyły 1,5-krotność pensji robotnika wykwalifikowanego [Zięba 1963]."
 print(sub(r"{{gap}}", r"(\1, \2)", txt))
+
+# exp output
+# 'Inflacja średnioroczna wzrosła wtedy do 40% (Henrykowski, 1962), a ceny bochenka chleba przekroczyły 1,5-krotność pensji robotnika wykwalifikowanego (Zięba, 1963).'
