127842affbf43ecb66adfd8e9ffa7d4d0a6a4e51
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:01:05 +0100

    Removed exercises/2cd033ff-b4b7-4a40-9b5c-762603a08194/solutions/e26b377b-ca22-4dee-a552-527cf90fbdfc/sol.py

--- exercises/2cd033ff-b4b7-4a40-9b5c-762603a08194/solutions/e26b377b-ca22-4dee-a552-527cf90fbdfc/sol.py
+++
@@ -1,8 +0,0 @@
-from re import split
-txt="""Rozdział I
-Historia się zaczyna. Tu nic się nie dzieje. Ale jest to fajnie opisane.
-Rozdział II
-Historia się rozwija. Tu też nic się nie dzieje. Ale jest to bardzo fajnie opisane.
-Rozdział III
-Historia się kończy. Tu już zupełnie nic się nie dzieje. Ale nawet to jest fajnie opisane."""
-print(split(r"Rozdział I+\n", txt)[1:])
