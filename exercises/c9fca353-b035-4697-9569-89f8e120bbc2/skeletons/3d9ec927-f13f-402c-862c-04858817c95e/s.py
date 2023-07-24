07a661853304d137a19c82d275cc1b819b8fd865
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:41:20 +0100

    updated file exercises/c9fca353-b035-4697-9569-89f8e120bbc2/skeletons/3d9ec927-f13f-402c-862c-04858817c95e/s.py

--- exercises/c9fca353-b035-4697-9569-89f8e120bbc2/skeletons/3d9ec927-f13f-402c-862c-04858817c95e/s.py
+++ exercises/c9fca353-b035-4697-9569-89f8e120bbc2/skeletons/3d9ec927-f13f-402c-862c-04858817c95e/s.py
@@ -1,3 +1,6 @@
 from re import sub
 txt = "makrela roleta sfinks pościel tryb woda kola koala buch"
 print(sub(r"{{gap}}", r"\1", txt))
+
+# exp output
+# makrel rolet sfinks pościel tryb wod kol koal buch
