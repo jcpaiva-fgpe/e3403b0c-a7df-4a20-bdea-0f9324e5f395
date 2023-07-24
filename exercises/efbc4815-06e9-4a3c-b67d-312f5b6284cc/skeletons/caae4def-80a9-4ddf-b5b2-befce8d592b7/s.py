a4c04337ba5609a1524d4c48d168a63a8c90471e
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 10:10:47 +0100

    updated file exercises/efbc4815-06e9-4a3c-b67d-312f5b6284cc/skeletons/caae4def-80a9-4ddf-b5b2-befce8d592b7/s.py

--- exercises/efbc4815-06e9-4a3c-b67d-312f5b6284cc/skeletons/caae4def-80a9-4ddf-b5b2-befce8d592b7/s.py
+++ exercises/efbc4815-06e9-4a3c-b67d-312f5b6284cc/skeletons/caae4def-80a9-4ddf-b5b2-befce8d592b7/s.py
@@ -2,3 +2,9 @@ from re import finditer
 txt = "chętnie rzekłbym coś niecałkiem ckliwego ani czcigodnego"
 m = finditer(r"{{gap}}",txt)
 for mo in m: print(mo.group(0), mo.span()[0], mo.span()[1]-mo.span()[0])
+
+# exp output
+# coś 17 3
+# niecałkiem 21 10
+# ckliwego 32 8
+# czcigodnego 45 11
