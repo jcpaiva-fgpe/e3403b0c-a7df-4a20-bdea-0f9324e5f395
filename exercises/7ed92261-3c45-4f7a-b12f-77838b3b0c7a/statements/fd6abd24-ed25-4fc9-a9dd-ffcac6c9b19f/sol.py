2042c742e2433511aeccbed7a6a3217554e104e1
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 15:28:43 +0100

    updated file exercises/7ed92261-3c45-4f7a-b12f-77838b3b0c7a/statements/fd6abd24-ed25-4fc9-a9dd-ffcac6c9b19f/sol.py

--- exercises/7ed92261-3c45-4f7a-b12f-77838b3b0c7a/statements/fd6abd24-ed25-4fc9-a9dd-ffcac6c9b19f/sol.py
+++ exercises/7ed92261-3c45-4f7a-b12f-77838b3b0c7a/statements/fd6abd24-ed25-4fc9-a9dd-ffcac6c9b19f/sol.py
@@ -1,6 +1,2 @@
 <b>Wielokrotność nieopcjonalna</b>
 <p>Znajdź pierwszą pozycję, na której pojawia się fraza zaczynająca się od 'K' a kończąca na 'i', zawierająca co najmniej 3 znaki.</p>
-<pre>
-# exp output
-# 0
-</pre>
