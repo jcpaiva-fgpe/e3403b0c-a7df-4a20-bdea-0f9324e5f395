f21ae7128aae691793b730319f456f56a9f03935
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 09:34:35 +0100

    created file exercises/74f79ae1-9ddb-4fe6-880a-752448371392/solutions/e330073c-5618-4673-bfae-9d0a5507104b/sol.py

---
+++ exercises/74f79ae1-9ddb-4fe6-880a-752448371392/solutions/e330073c-5618-4673-bfae-9d0a5507104b/sol.py
@@ -0,0 +1,7 @@
+from re import sub
+txt = """1. Rębajło, Heliodor - 18,5 s
+2. Dziureczko, Apolinary - 19,2 s
+3. Truteń, Szymon - 19,3 s
+4. Dziamdziara, Gleb - 20,1 s
+5. Skowronek, Zdzisław - 20,5 s"""
+print(sub(r"([A-Z]\w+),\s([A-Z]\w+)", r"\2 \1", txt))
