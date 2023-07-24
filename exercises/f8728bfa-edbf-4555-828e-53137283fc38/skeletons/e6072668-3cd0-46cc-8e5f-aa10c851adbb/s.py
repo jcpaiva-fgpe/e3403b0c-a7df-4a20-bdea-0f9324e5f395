0c9e810754a0b52f4741d561510452e9775039b6
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 10:46:05 +0100

    updated file exercises/f8728bfa-edbf-4555-828e-53137283fc38/skeletons/e6072668-3cd0-46cc-8e5f-aa10c851adbb/s.py

--- exercises/f8728bfa-edbf-4555-828e-53137283fc38/skeletons/e6072668-3cd0-46cc-8e5f-aa10c851adbb/s.py
+++ exercises/f8728bfa-edbf-4555-828e-53137283fc38/skeletons/e6072668-3cd0-46cc-8e5f-aa10c851adbb/s.py
@@ -1,3 +1,6 @@
 from re import sub
 txt = "Agent 7:3 zadzwonił pod numer 997:4 i usłyszał 1:3"
 print(sub(r"{{gap}}", lambda m: "0"*(int(m.group(2))-len(m.group(1)))+m.group(1), txt))
+
+# exp output
+# Agent 007 zadzwonił pod numer 0997 i usłyszał 001
